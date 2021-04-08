69d183004864adc067e9cf5b8b3e7bfeefa9306d
Pratik Patre <56273302+fullsnack-DEV@users.noreply.github.com>
2021-04-09 01:28:54 +0530

    Commit

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 //Functions
 
+//Differnce Between Regular and Arrow function
+
+//The Arrow function does ot get its OWn this Keyword
+//Its simply use this keyword from its surrounding
+
+//i.e The Arrow function inherits the this Keyword from the
+//parent Scope
+
+// const specs = {
+//   firstname: "Prateek",
+//   year: 2028,
+
+//   calcaage: function (birthyr) {
+//     return this.year - birthyr;
+//   }
+// };
+
+// const age1 = specs.calcaage(1996);
+
+// console.log(age1);
+
 // //Difference Between Function Decalaration and Function Expression
 
 // ***************** Function Decalration //   **************//
